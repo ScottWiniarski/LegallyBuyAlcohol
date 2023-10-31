52db1c759baec9721ce68573fa7d40b8ba6105fd
Scott Winiarski <Scott1w1@hotmail.com>
2023-10-30 19:42:34 -0500

    Finished 3 of four lab requirements

--- LegallyBuyAlcohol/MainPage.xaml.cs
+++ LegallyBuyAlcohol/MainPage.xaml.cs
@@ -14,5 +14,25 @@ namespace LegallyBuyAlcohol
         {
             InitializeComponent();
         }
+
+        void OnDateSelected(object sender, System.EventArgs e)
+        {
+            //TimeSpan timeSpan = DateTime.Now - birthDate.Date;
+            double UsAge = DateTime.Now.Year - birthDate.Date.Year;
+            Out_text.Text = UsAge.ToString();
+            if (UsAge >= 21 || UsAge == 0)
+            {
+                Out_text.Text = "It's legal to drink in the United States.";
+            }
+            else
+            {
+                Out_text.Text = "You've got " + (DateTime.Now.Year - birthDate.Date.Year) + " Year(s) to go";
+                //Out_text.Text = timeSpan.TotalDays.ToString() + " + " + timeSpan.Days.ToString();
+            }
+            //Out_text.Text = birthDate.Date.Year.ToString();
+
+            //Out_text.Text = $"Hello {DateTime.Now - birthDate.Date}";
+
+        }
     }
 }
